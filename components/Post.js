c3c6f3e172b7196c57fb68cd69707ea8da036135
dejan-stankovic-8033 <dejanstankovic8033@gmail.com>
2020-07-06 14:25:04 +0500

    Fix issues for featuredImage.sourceUrl on post page

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import clientConfig from "../client-config";
 
 const Post = props => {
   const { post } = props;
-
+  
   return undefined !== post ? (
     <div className="w-1/2 md:w-1/4 lg:w-1/5 px-3 pb-8" key={post.postId}>
       <Link
@@ -11,7 +11,7 @@ const Post = props => {
         href={`/post?slug=${post.slug}-${post.postId}`}
       >
         <a className="block mb-2">
-          <img src={post.featuredImage.sourceUrl} alt={post.title} />
+          <img src={post.featuredImage? post.featuredImage.sourceUrl: ``} alt={post.title} />
         </a>
       </Link>
       <Link
